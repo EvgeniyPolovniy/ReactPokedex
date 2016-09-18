8eb22b4925819aefa14d90e36b76451e370990f3
Евгений <derron@EP.local>
2016-09-18 11:35:46 +0300

    Add https

--- src/AC/pokemons.js
+++ src/AC/pokemons.js
@@ -3,6 +3,6 @@ import { LOAD_POKEMONS } from '../../constants'
 export function loadPokemons() {
   return {
     type: LOAD_POKEMONS,
-    callAPI: 'http://pokeapi.co/api/v1/pokemon/?limit=10'
+    callAPI: 'https://pokeapi.co/api/v1/pokemon/?limit=10'
   }
 }
